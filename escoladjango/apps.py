83b3bfbf27a5ea47f0bc9bd65740c6107449f4ee
hiago rodrigues <ti7@facfama.edu.br>
2025-05-26 16:29:25 -0300

    commit incial do curso de Django Alura 5 dias

---
+++ escoladjango/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EscoladjangoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "escoladjango"
---
+++ escoladjango/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+
+def estudante(request):
+    """
+    Visualização para lidar com solicitações de dados do aluno.
+    Retorna uma resposta JSON com informações do aluno.
+    """
+    if request.method != 'GET':
+        return JsonResponse({'error': 'Método não permitido'}, status=405)
+    else:
+        # Example student data
+        student_data = {
+            "name": "John Doe",
+            "age": 20,
+            "course": "Computer Science"
+        }
+    
+        return JsonResponse(student_data)
