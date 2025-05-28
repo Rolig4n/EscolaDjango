abb92282f75e0989f79bbe1bf4d97caa6add9d38
hiago rodrigues <ti7@facfama.edu.br>
2025-05-28 14:15:09 -0300

    codigo da aula 2

--- escoladjango/serializers.py
+++ escoladjango/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from escoladjango.models import Estudante, Curso, Matricula
+from escoladjango.validators import cpf_invalido, nome_invalido, celular_invalido
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
         fields = '__all__'
+    
+    def validate(self, dados):
+        if cpf_invalido(dados['cpf']):
+            raise serializers.ValidationError({'cpf':"deve conter apenas números e ter 11 dígitos."})
+        if nome_invalido(dados['nome']):
+            raise serializers.ValidationError({'nome':"deve conter apenas letras."})
+        if celular_invalido(dados['numero_celular']):
+            raise serializers.ValidationError({'numero_celular':"deve conter apenas números e ter 14 dígitos."})
+        return dados
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
